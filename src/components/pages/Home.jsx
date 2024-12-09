f18284e572828d576851a6dd3b84b7b8fe811575
void-57 <ruddhaani60@gmail.com>
2024-12-10 03:23:43 +0530

    feat: add Open Graph and Twitter card meta tags to index.html for improved SEO

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Container, PostCard } from "..";
 import service from "../../appwrite/configuration";
-import { Helmet } from "react-helmet";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -16,14 +15,8 @@ function Home() {
     }
   }, [authStatus]);
 
-  const pageTitle = "✍️ Your ❤️ Out - A Blogging Platform for Writers";
-  const pageDescription =
-    "WriteYour❤️Out is your go-to platform for creating, editing, and managing blog posts. View posts from others, share your own, and connect with a community of writers.";
-  const pageUrl = "https://writeyourheartout.vercel.app/";
-  const pageImage =
-    "https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/8103728/724163_416084.png";
 
-  
+
   if (!authStatus) {
     return (
       <div className="w-full py-8 mt-4 text-center">
@@ -57,22 +50,7 @@ function Home() {
   }
 
   return (
-    <>
-    <Helmet>
-        <title>{pageTitle}</title>
-        <meta property="og:title" content={pageTitle} />
-        <meta property="og:description" content={pageDescription} />
-        <meta property="og:image" content={pageImage} />
-        <meta property="og:url" content={pageUrl} />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={pageTitle} />
-        <meta name="twitter:description" content={pageDescription} />
-        <meta name="twitter:image" content={pageImage} />
-      </Helmet>
-
     <div className="py-8 w-full">
-    
       <Container>
         <div className="flex flex-wrap">
           {posts.map((post) => (
@@ -86,7 +64,6 @@ function Home() {
         </div>
       </Container>
     </div>
-    </>
   );
 }
 
